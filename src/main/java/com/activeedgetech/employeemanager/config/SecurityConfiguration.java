e348614c027a4a586ce48c4e39cead56d335de0b
princelewis <princelewis4christ@yahoo.com>
2021-05-02 00:11:32 +0100

    Modify Security configuration to enable Jpa auditing

--- src/main/java/com/activeedgetech/employeemanager/config/SecurityConfiguration.java
+++ src/main/java/com/activeedgetech/employeemanager/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.activeedgetech.employeemanager.security.SecurityAuthenticationEntryPo
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableJpaAuditing
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
