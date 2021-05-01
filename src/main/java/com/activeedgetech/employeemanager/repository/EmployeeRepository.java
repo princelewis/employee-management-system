e5ee33a457f974b5cc949a09857ac50275f76009
princelewis <princelewis4christ@yahoo.com>
2021-05-02 00:30:07 +0100

    Create a repository for accessing employee entity

---
+++ src/main/java/com/activeedgetech/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.activeedgetech.employeemanager.repository;
+
+import com.activeedgetech.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
