d333ba54317079a0b6c126830be41010e783e835
princelewis <princelewis4christ@yahoo.com>
2021-05-03 08:43:07 +0100

    Modify App constants to house new constants

--- src/main/java/com/activeedgetech/employeemanager/util/AppConstant.java
+++ src/main/java/com/activeedgetech/employeemanager/util/AppConstant.java
@@ -4,4 +4,5 @@ public interface AppConstant {
     String JWT_SUCCESS_MESSAGE = "Token generated";
     String EMPLOYEE_CREATION_SUCCESS_MESSAGE = "Employee successfully created";
     String EMPLOYEE_UPDATE_SUCCESS_MESSAGE = "Employee information successfully updated";
+    String FETCH_ALL_EMPLOYEE_MESSAGE = "All employee data fetched";
 }
